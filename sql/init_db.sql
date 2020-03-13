334ec927bae1d492b1305680744f2af626116263
Florian Krull <flo.krull@gmail.com>
2020-03-13 14:07:20 +0100

    sql: create view - combined core tables

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -1,4 +1,5 @@
 
+DROP VIEW IF EXISTS core;
 -- drop tables if they exist
 -- creates a script to delete all long_* tables
 DO
@@ -71,14 +72,39 @@ CREATE TABLE metatables (
   CONSTRAINT metatables_pk PRIMARY KEY(id)
 );
 
-
 -- Create table of tables
 
 CREATE TABLE metacolumns (
   metatable_id text,
   id text,
+  rank integer DEFAULT 0,
   name text,
   CONSTRAINT metacolumns_pk PRIMARY KEY (metatable_id, id),
   CONSTRAINT metacolumns_fk FOREIGN KEY (metatable_id) REFERENCES metatables(id)
 );
 
+-- Combine core tables
+
+CREATE VIEW core AS
+  SELECT
+    subjects.id        AS subject_id,
+    subjects.birthdate AS subject_birthdate,
+    subjects.sex       AS subject_sex,
+    subjects.shareable AS subject_shareable,
+    visits.visitdate   AS visit_visitdate,
+    waves.reknr        AS wave_reknr,
+    waves.description  AS wave_description,
+    waves.code         AS wave_code,
+    projects.id          AS project_id,
+    projects.name        AS project_name,
+    projects.code        AS project_code,
+    projects.description AS project_desciption
+  FROM
+    visits
+  LEFT OUTER JOIN waves ON
+    visits.wave_code = waves.code
+    and visits.project_id = waves.project_id
+  LEFT OUTER JOIN subjects ON
+    visits.subject_id = subjects.id
+  LEFT OUTER JOIN projects ON
+    visits.project_id = projects.id
