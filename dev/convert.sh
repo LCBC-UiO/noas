88a7698287358fb4faa94c4663831318035f1906
Florian Krull <flo.krull@gmail.com>
2020-03-15 17:07:09 +0100

    webui: add favicon

--- dev/convert.sh
+++ dev/convert.sh
@@ -1,1 +1,2 @@
 convert logo_noshadow.png -define icon:auto-resize=64,48,32,16 favicon.ico
+convert logo_noshadow.png -define icon:auto-resize=64,48,32,16 -unsharp 2x1.0+0.9+0 ../webui/static/img/favicon.ico
