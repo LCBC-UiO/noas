17cfadb6cecf22bf759452dea4b1c21840ea3fea
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-04-23 21:47:53 +0200

    add timer for adding the data-base files

--- r/populate_db.R
+++ r/populate_db.R
@@ -8,9 +8,21 @@ con <- moasdb_connect()
 j <- DBI::dbExecute(con, 
                     read_sql("sql/init_db.sql"))
 
+start <- Sys.time()
+
 populate_core(con)
 populate_table("long", con)
 populate_table("repeated", con)
 populate_table("cross", con)
 
+spent <- as.numeric(round(Sys.time() - start, 3))
+spent <- dplyr::case_when(
+  spent < 10 ~ crayon::green(spent),
+  spent > 30 ~ crayon::red(spent),
+  TRUE ~ crayon::yellow(spent)
+)
+
+cat("\n ---------- \n")
+cat_table_success(TRUE,
+                  crayon::bold("Database populated in", spent, "minutes"))
 
