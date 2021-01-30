b56c3f719e3d721bb5638f78dfeb6bb401bbb031
Florian Krull <flo.krull@gmail.com>
2021-01-30 18:52:24 +0100

    dbimport: insert_metadata support optional column type

--- dbimport/funcs-metadata.R
+++ dbimport/funcs-metadata.R
@@ -39,11 +39,19 @@ insert_metadata <- function(meta_info, con){
           )
         }
       }
-      # add metacolumns
-      if(all(c(!is.null(meta_info$jsn$columns), 
-              nrow(meta_info$jsn$columns) > 0))){
-        if (!alter_cols(meta_info, con)) {
-          stop("insert_metadata column type");
+      # loop over all columns - if columns is NULL, this will do nothing
+      for (i in 1:length(meta_info$jsn$columns)) {
+        mc <- meta_info$jsn$columns[[i]]
+        # needs id
+        if (is.null(mc[["id"]])) {
+          stop(sprintf("insert_metadata table=%s missing id in column=%d",
+            meta_info$id,
+            i
+          ))
+        }
+        # set column type?
+        if (!is.null(mc[["type"]])) {
+          alter_col(con, meta_info$id, meta_info$table_type, mc$id, mc$type)
         }
       }
       invisible(T)
@@ -55,26 +63,17 @@ insert_metadata <- function(meta_info, con){
   invisible(ok)
 }
 
-alter_cols <- function(meta_info, con){
-  sql_tab = sprintf("ALTER TABLE %s_%s", 
-                    meta_info$table_type, meta_info$id)
-  
-  sql_cols <- mapply(
-    sprintf, 
-    meta_info$jsn$columns$id,
-    meta_info$jsn$columns$type,
-    meta_info$jsn$columns$id,
-    meta_info$jsn$columns$type,
-    MoreArgs = list(
-      # Need the USING part because all columns are imported as string at first
-      # https://stackoverflow.com/questions/13170570/change-type-of-varchar-field-to-integer-cannot-be-cast-automatically-to-type-i
-      fmt = 'ALTER COLUMN "_%s" TYPE %s USING (_%s::%s)'
-    )
+alter_col <- function(con, table_id, table_type, col_id, col_type){
+  # Need the USING part because all columns are imported as string at first
+  # https://stackoverflow.com/questions/13170570/change-type-of-varchar-field-to-integer-cannot-be-cast-automatically-to-type-i
+  sql_cmd <- sprintf('ALTER TABLE %s_%s ALTER COLUMN "_%s" TYPE %s USING (_%s::%s);',
+    table_type,
+    table_id,
+    col_id,
+    col_type,
+    col_id,
+    col_type
   )
-  sql_cmd <- paste(sql_tab, 
-                   paste(sql_cols, collapse = ", "), 
-                   ";", sep = " ")
-
   k <- DBI::dbExecute(con, sql_cmd)
   k <- ifelse(k == 0, TRUE, FALSE) # the alter statement seems to update 0 rows
   invisible(k)
