bbe8c1c95515b6198f9a69513cf0004622c2f763
Florian Krull <flo.krull@gmail.com>
2021-03-21 17:24:02 +0100

    dbimport: skip missing metadata

--- dbimport/funcs-metadata.R
+++ dbimport/funcs-metadata.R
@@ -8,6 +8,7 @@ read_metadata_str <- function(dirpath){
 
 fix_metadata <- function(table_dir, con) {
   jsn_str <- read_metadata_str(table_dir);
+  if (is.null(jsn_str)) return()
   table_id <- basename(table_dir)
   DBI::dbExecute(con, "select import_metadata($1, $2);", params=list(table_id, jsn_str))
   cat_table_success(j, sprintf("metadata\t%s\tadded\t ", basename(table_dir)))
