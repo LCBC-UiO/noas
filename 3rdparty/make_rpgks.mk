bfcf1175124f3e37e4d01b70611f50a08b697d4a
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2023-02-28 12:13:36 +0100

    revert rpack versions for rhel7

--- 3rdparty/make_rpgks.mk
+++ 3rdparty/make_rpgks.mk
@@ -11,8 +11,8 @@ _all: all
 # note: the non-Archive urls will break over time. this wil need updates
 r_pkg_urls := https://cran.r-project.org/src/contrib/00Archive/digest/digest_0.6.30.tar.gz \
 	https://cran.r-project.org/src/contrib/00Archive/jsonlite/jsonlite_1.8.3.tar.gz \
-	https://cran.r-project.org/src/contrib/00Archive/DBI/DBI_1.1.2.tar.gz \
-	https://cran.r-project.org/src/contrib/00Archive/RPostgreSQL/RPostgreSQL_0.7-4.tar.gz 
+	https://cran.r-project.org/src/contrib/00Archive/DBI/DBI_1.1.0.tar.gz \ # version for RPostgreSQL
+	https://cran.r-project.org/src/contrib/00Archive/RPostgreSQL/RPostgreSQL_0.6-2.tar.gz #rhel7 compatible 
 
 r_pkgs := $(patsubst %.tar.gz,%, $(notdir $(r_pkg_urls)))
 
