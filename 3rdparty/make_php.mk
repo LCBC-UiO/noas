88c30ab9d0e81e10dbb36498ede648c147dcc452
Inge Amlien <iamlien@gmail.com>
2024-05-24 10:11:38 +0000

    Add xdebug to make_php, vscode xdebug launch prof Add vscode xdebug extension to .devcontainer.json

--- 3rdparty/make_php.mk
+++ 3rdparty/make_php.mk
@@ -7,7 +7,7 @@ LIBVER_php := 7.4.6
 php_download: download/php-${LIBVER_php}.tar.gz
 
 .PHONY: php_build
-php_build: php/bin/php-cgi
+php_build: php/bin/php-cgi php/lib/php/extensions/no-debug-non-zts-20190902/xdebug.so
 
 .PHONY: php_clean
 php_clean:
@@ -31,3 +31,19 @@ php/bin/php-cgi: download/php-${LIBVER_php}.tar.gz | postgresql_build
 	$(MAKE) -C php_build/php-$(LIBVER_php)
 	$(MAKE) -C php_build/php-$(LIBVER_php) install
 	$(RM) -r php_build
+
+# Change xdebug version if updating php version (https://xdebug.org/wizard)
+download/xdebug-3.1.6.tgz:
+	mkdir -p download/
+	cd download && wget https://xdebug.org/files/xdebug-3.1.6.tgz
+
+php/lib/php/extensions/no-debug-non-zts-20190902/xdebug.so: php/bin/php-cgi download/xdebug-3.1.6.tgz
+	mkdir -p $(BASEDIR)/php_xdebug_build \
+		&& cd $(BASEDIR)/php_xdebug_build \
+		&& tar xf $(BASEDIR)/download/xdebug-3.1.6.tgz \
+		&& cd $(BASEDIR)/php_xdebug_build/xdebug-3.1.6 \
+		&& $(BASEDIR)/php/bin/phpize \
+		&& ./configure --with-php-config=$(BASEDIR)/php/bin/php-config
+	$(MAKE) -C $(BASEDIR)/php_xdebug_build/xdebug-3.1.6
+	cp $(BASEDIR)/php_xdebug_build/xdebug-3.1.6/modules/xdebug.so $(BASEDIR)/php/lib/php/extensions/no-debug-non-zts-20190902/
+	$(RM) -r $(BASEDIR)/php_xdebug_build
